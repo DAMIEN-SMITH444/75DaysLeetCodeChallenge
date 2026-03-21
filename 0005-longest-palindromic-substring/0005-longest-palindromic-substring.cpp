732c77776ba6a3c800f17c1c45f0059014b96b73
.DAMIEN <155760727+DAMIEN-SMITH444@users.noreply.github.com>
2026-03-22 00:53:30 +0530

    0005-LONGEST-PALINDROMIC-SUBSTRING Stats: Time: 218 ms (17.67%), Space: 13.9 MB (45.93%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
+    int dp[1001][1001];
     bool solve(string &s , int i , int j){
-        if(i >= j )return true;
+        if(i >= j )return true; 
 
+        if(dp[i][j] != -1)return dp[i][j];
         if(s[i] == s[j]){
-            return solve(s,i+1,j-1);
+            return dp[i][j] = solve(s,i+1,j-1);
         }
         return false;
     }
     string longestPalindrome(string s) {
         int n = s.size();
-
+        memset(dp,-1,sizeof(dp));
         int maxlen = INT_MIN;
         int sp = 0;
         for(int i = 0; i < n ; i++){
