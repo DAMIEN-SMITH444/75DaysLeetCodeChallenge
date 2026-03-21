5f4f36115799a59f3d62e8bb34a054822e5422eb
.DAMIEN <155760727+DAMIEN-SMITH444@users.noreply.github.com>
2026-03-22 01:05:24 +0530

    0005-LONGEST-PALINDROMIC-SUBSTRING Stats: Time: 211 ms (18%), Space: 26 MB (31.01%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,8 +1,45 @@
 class Solution {
 public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        vector<vector<bool>> t(n, vector<bool>(n));
+
+        for (int i = 0; i < n; i++) {
+            t[i][i] = true;
+        }
+        int maxL = 1;
+        int idx = 0;
+        for (int l = 2; l <= n; l++) {
+            for (int i = 0; i < n - l + 1; i++) {
+                int j = i + l - 1;
+                if (s[i] == s[j] && l == 2) {
+                    t[i][j] = true;
+                    maxL = 2;
+                    idx = i;
+                } else if (s[i] == s[j] && t[i + 1][j - 1] == true) {
+                    t[i][j] = true;
+                    if (maxL < j - i + 1) {
+                        maxL = j - i + 1;
+                        idx = i;
+                    }
+                }
+
+                else
+                    t[i][j] = false;
+            }
+        }
+        return s.substr(idx, maxL);
+    }
+};
+
+/*
+recursion + memoization
+
+class Solution {
+public:
     int dp[1001][1001];
     bool solve(string &s , int i , int j){
-        if(i >= j )return true; 
+        if(i >= j )return true;
 
         if(dp[i][j] != -1)return dp[i][j];
         if(s[i] == s[j]){
@@ -28,3 +65,5 @@ public:
         return s.substr(sp,maxlen);
     }
 };
+
+*/
